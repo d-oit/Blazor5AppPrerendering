630b2efd9e33609525582ecffa775bbb9084618b
Dominik Oswald <6849456+do-it-ger@users.noreply.github.com>
2021-01-05 11:01:12 +0100

    Add project files.

---
+++ Blazor5AppPrerendering/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Blazor5AppPrerendering.Server.Data;
+using Blazor5AppPrerendering.Server.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Blazor5AppPrerendering.Server.Areas.Identity.IdentityHostingStartup))]
+namespace Blazor5AppPrerendering.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
